0d59b429feb5b409416288eb66aec75b6ed98ac9
f0X231 <guChai@gmail.com>
2018-10-25 09:21:17 +0700

    all pages

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name=viewport content="width=device-width, initial-scale=1">
+	<meta property="og:title" content="" />
+	<meta property="og:image" content="" />
+	<meta property="og:site_name" content="" />
+	<meta property="og:description" content="" />
+	<meta name="robot" content="noindex, nofollow" />
+	<title>Title of the document</title>
+
+	<link rel="stylesheet" type="text/css" href="src/css/theme.css">
+</head>
+
+<body>
+	<header>
+		<div class="container is-widescreen">
+			<div class="columns">
+				<div class="column">1</div>
+				<div class="column">2</div>
+			</div>
+
+			<div class="columns">
+				<div class="column">1</div>
+				<div class="column">2</div>
+				<div class="column">3</div>
+				<div class="column">4</div>
+				<div class="column">5</div>
+				<div class="column">6</div>
+				<div class="column">7</div>
+				<div class="column">8</div>
+			</div>
+		</div>
+	</header>
+
+	<footer>
+		<div class="container is-widescreen">
+			<div class="columns">
+				<div class="column is-three-quarters">
+					<div class="columns">
+						<div class="column">Travel Professional</div>
+						<div class="column">FAQ</div>
+						<div class="column">Contact Us</div>
+						<div class="column">JNTO Head Office</div>
+						<div class="column">Site Map</div>
+					</div>
+					<div class="columns">
+						<div class="column">องค์การส่งเสริมการท่องเที่ยวแห่งประเทศญี่ปุ่น</div>
+						<div class="column">FAQ</div>
+					</div>
+				</div>
+				<div class="column">2</div>
+			</div>
+		</div>
+	</footer>
+
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="node_modules/jquery-lang-js/js/jquery-lang.js"></script>
+    <script src="src/js/scripts.js"></script>
+    
+</body>
+
+</html>
