9f0a28afb64293ec06df0c76e88de23f37cf93f6
chai <apichai@ewitewit.com>
2018-10-25 14:24:52 +0700

    font

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,25 @@
 
-jQuery(document).ready(function() {
+function updateText() {
+    var i18n = $.i18n();
+    //$(this).addClass('active');
+    var language = $(this).data('options').lang;
 
+    i18n.locale = language;
+    i18n.load('./src/lang/' + language + '.json', language).done(function() {
+        $('.result').text(localizedMessage);
+    });
+    
+}
+
+function switchLanguage() {
+    $('.switch-lang').on('click', updateText);
+}
+
+
+
+$.i18n.debug = true;    // Enable|Disable debuging of switch language.
+jQuery(document).ready(function() {
+    switchLanguage();
 });
+
+
